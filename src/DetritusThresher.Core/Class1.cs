233af0604b37bae60df38df88007d6797974f7ff
Thomas Harold <tgharold@gmail.com>
2019-02-12 23:49:36 -0500

    setup C# .NET Core project/solution

---
+++ src/DetritusThresher.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DetritusThresher.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/DetritusThresher.Migrations/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DetritusThresher.Migrations
+{
+    public class Class1
+    {
+    }
+}
