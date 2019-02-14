166dbe36512ee11ad2d4f153176d2ecb34223690
Thomas Harold <tgharold@gmail.com>
2019-02-14 03:39:32 -0500

    setting up initial migration

---
+++ src/DetritusThresher.Migrations/Migrations/20190214033700_InitialMigration.cs
@@ -0,0 +1,13 @@
+using FluentMigrator;
+
+namespace DetritusThresher.Migrations.Migrations
+{
+    [Migration(20190214033700)]
+    public class InitialMigration : ForwardOnlyMigration
+    {
+        public override void Up()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
