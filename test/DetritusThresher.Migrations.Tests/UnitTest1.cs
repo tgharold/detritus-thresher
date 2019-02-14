96349090cba79979fb6dee65aea2c0d43e2ac5a7
Thomas Harold <tgharold@gmail.com>
2019-02-14 06:45:25 -0500

    add migration tests folder

---
+++ test/DetritusThresher.Migrations.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DetritusThresher.Migrations.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
