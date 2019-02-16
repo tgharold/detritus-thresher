11d445ee686042855338da705891d99b186d4e15
Thomas Harold <tgharold@gmail.com>
2019-02-16 07:09:45 -0500

    refactoring

--- test/DetritusThresher.Testing/Fixtures/SqliteFixture.cs
+++ test/DetritusThresher.Testing/Fixtures/SqliteFixture.cs
@@ -1,7 +1,15 @@
+using Microsoft.Extensions.DependencyInjection;
+using FluentMigrator;
+using System;
+using Xunit;
+
 namespace DetritusThresher.Testing.Fixtures
 {
     public class SqliteFixture
     {
-        
+        private ServiceCollection CreateServiceCollection()
+        {
+            return new ServiceCollection();
+        }
     }
 }
