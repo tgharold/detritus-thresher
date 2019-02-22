bddfc6a62f9447c54a3cea72bcb47b097f0e4cfd
Thomas Harold <tgharold@gmail.com>
2019-02-22 18:06:03 -0500

    patch up test

--- test/DetritusThresher.Core.Tests/Models/ScanTests.cs
+++ test/DetritusThresher.Core.Tests/Models/ScanTests.cs
@@ -36,7 +36,7 @@ namespace DetritusThresher.Core.Tests.Models
             using (var db = new NPoco.Database(connection, DatabaseType.SQLite))
             {
                 db.Insert(scan);
-                var result = db.SingleById<Scan>(scan.ScanId);
+                var result = db.SingleById<Scan>(scan.Id);
                 Assert.Equal(name, result.Name);
             }
         }
@@ -47,6 +47,10 @@ namespace DetritusThresher.Core.Tests.Models
             // Microsoft.Data.Sqlite (as of 2.2.2) has problems storing/retrieving DateTime values as kind UTC
             // https://system.data.sqlite.org/ -- works fine and returns UTC timestamp
 
+            // Update: NPoco/System.Data.SQLite seems to have trouble round-tripping UTC time to the database
+            // The fix for this is adding "datetimekind=Utc" to the connetion string
+            // "data source=\"file:mem-073d7ec1-528a-4482-8264-caf718d4fc06?mode=memory&cache=shared\";datetimekind=Utc"
+
             var name = $"test message: {nameof(CanGetBackKindUtcFromCreated)} {DateTimeOffset.UtcNow.Ticks}";
             var scan = ModelBuilders.CreateScan(name);
             scan.ScanFinished = new DateTime(2018, 12, 13, 22, 55, 43, DateTimeKind.Utc);
@@ -54,9 +58,9 @@ namespace DetritusThresher.Core.Tests.Models
             using (var db = new NPoco.Database(connection, DatabaseType.SQLite))
             {
                 db.Insert(scan);
-                var result = db.SingleById<Scan>(scan.ScanId);
-                Assert.Equal(DateTimeKind.Utc, result.ScanCreated.Kind);
+                var result = db.SingleById<Scan>(scan.Id);
                 Assert.Equal(DateTimeKind.Utc, result.ScanFinished?.Kind);
+                Assert.Equal(scan.ScanFinished, result.ScanFinished);
             }
         }
     }
