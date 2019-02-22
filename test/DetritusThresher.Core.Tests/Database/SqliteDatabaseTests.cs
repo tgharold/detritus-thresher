144a1f0dabcd865e3420a95c26598ed647786552
Thomas Harold <tgharold@gmail.com>
2019-02-22 07:39:06 -0500

    disable xUnit warnings about Dispose()

--- test/DetritusThresher.Core.Tests/Database/SqliteDatabaseTests.cs
+++ test/DetritusThresher.Core.Tests/Database/SqliteDatabaseTests.cs
@@ -16,10 +16,12 @@ namespace DetritusThresher.Core.Tests.Database
             connection.Open();
         }
 
+#pragma warning disable xUnit1013 // Public method should be marked as test
         public void Dispose()
         {
             connection.Dispose();
         }
+#pragma warning restore xUnit1013 // Public method should be marked as test
 
         [Fact]
         public void CanReadVersionTable()
--- test/DetritusThresher.Core.Tests/Models/LogEntryTests.cs
+++ test/DetritusThresher.Core.Tests/Models/LogEntryTests.cs
@@ -19,10 +19,12 @@ namespace DetritusThresher.Core.Tests.Models
             connection.Open();
         }
 
+#pragma warning disable xUnit1013 // Public method should be marked as test
         public void Dispose()
         {
             connection.Dispose();
         }
+#pragma warning restore xUnit1013 // Public method should be marked as test
 
         [Fact]
         public void CanCreateAndSave()
