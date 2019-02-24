a29cf049976edc619af571488f485e8fa6370620
Thomas Harold <tgharold@gmail.com>
2019-02-24 10:33:15 -0500

    dotnet new console --name DetritusThresher.ConsoleScanner

---
+++ src/DetritusThresher.ConsoleScanner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DetritusThresher.ConsoleScanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
